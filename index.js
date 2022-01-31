9acc6556d754220ec0b497018b4776c7b0a77b83
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-01-31 22:30:50 +0100

    Add alert on form submission

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-document.querySelectorAll(".contactForm").forEach((element) => {
-  element.addEventListener("submit", (event) => {
+document.querySelectorAll(".contactForm").forEach((formElement) => {
+  formElement.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    const email = [...formElement.children].find(isEmailInputElement).value;
+
+    formElement.appendChild(getSignedUpAlert(email));
   });
 });
+
+function getSignedUpAlert(email) {
+  const alertElement = document.createElement("div");
+  alertElement.innerHTML = `<p class="signedup-alert">Signed up${
+    email ? " with " + `<b>${email}</b>` : ""
+  }!</p>`;
+
+  return alertElement;
+}
+
+function isEmailInputElement(element) {
+  return (element.class = "email-input");
+}
